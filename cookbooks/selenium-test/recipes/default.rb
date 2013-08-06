2d1d40ebd59689ab31536844930383ba46cf43ed
Onkar Bapat <onkarbapat@gmail.com>
2013-08-06 22:31:11 +0200

    Install java and start selenium hub

--- cookbooks/selenium-test/recipes/default.rb
+++ cookbooks/selenium-test/recipes/default.rb
@@ -7,4 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "apache2"
+include_recipe "java"
+
+bash "download_selenium" do
+  user "root"
+  cwd "#{Chef::Config[:file_cache_path]}"
+  code <<-EOH
+wget https://selenium.googlecode.com/files/selenium-server-standalone-2.34.0.jar
+EOH
+not_if { File.exists?("/home/vagrant/chef-solo/selenium-server-standalone-2.34.0.jar") }
+end
+
+bash "start_hub" do
+  user "root"
+  cwd "#{Chef::Config[:file_cache_path]}"
+  code <<-EOH
+java -jar selenium-server-standalone-2.34.0.jar -role hub -port 4444 &
+EOH
+end
