60fab1b73ea3a944a3da0dae77526e80e9c482d9
Onkar Bapat <onkarbapat@gmail.com>
2013-08-17 00:44:53 +0200

    working role - HUB

--- cookbooks/selenium-test/recipes/hub.rb
+++ cookbooks/selenium-test/recipes/hub.rb
@@ -13,15 +13,15 @@ bash "download_selenium" do
   user "root"
   cwd "#{Chef::Config[:file_cache_path]}"
   code <<-EOH
-wget "#{node.selenium.url}"+"#{node.selenium.name}"+"#{node.selenium.version}"+".jar"
+wget "#{node.selenium.url}#{node.selenium.name}#{node.selenium.version}.jar"
 EOH
-not_if { File.exists?(File.join(Dir.pwd, 'cache')+"/"+"#{node.selenium.name}"+"#{node.selenium.version}"+".jar")}
+not_if { File.exists?(File.join(Dir.pwd, 'cache')+"/#{node.selenium.name}#{node.selenium.version}.jar")}
 end
 
 bash "start_hub" do
   user "root"
   cwd "#{Chef::Config[:file_cache_path]}"
   code <<-EOH
-java -jar "#{node.selenium.name}"+"#{node.selenium.version}"+".jar" -role hub -port 4444 &
+java -jar "#{node.selenium.name}#{node.selenium.version}.jar" -role hub -port 4444 &
 EOH
 end
--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
 file_cache_path File.join(Dir.pwd, 'cache')
 cookbook_path File.join(Dir.pwd, 'cookbooks')
-
+role_path File.join(Dir.pwd,'roles')
