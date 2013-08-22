3048a9fef22d903c7e06888c643f58c06f902ce5
Onkar Bapat <onkar_bapat@intuit.com>
2013-08-21 22:15:20 -0700

    selenium node

--- cookbooks/selenium-test/recipes/node.rb
+++ cookbooks/selenium-test/recipes/node.rb
@@ -6,11 +6,35 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "java"
+include_recipe "powershell"
 
-include_recipe "chocolatey"
-
-windows_batch "ffox" do
+powershell "download selenium" do
+  cwd Chef::Config[:file_cache_path]
   code <<-EOH
-  cinst Firefox
-  EOH
+  $webclient = New-Object System.Net.WebClient;
+$url = #{node.selenium.url}#{node.selenium.name}#{node.selenium.version}.jar;
+$file = #{node.selenium.name}#{node.selenium.version}.jar;
+$webclient.DownloadFile($url,$file);
+EOH
+  not_if { File.exists?(File.join(Dir.pwd, 'cache')+"/#{node.selenium.name}#{node.selenium.version}.jar")}
 end
+
+powershell "install browser" do 
+	code <<-EOH
+	cinst #{node.browser.package} #{node.browser.version}
+	EOH
+end
+
+powershell "start_node" do
+  cwd "#{Chef::Config[:file_cache_path]}"
+  code <<-EOH
+ Start-Process java.exe -ArgumentList "-jar #{node.selenium.name}#{node.selenium.version}.jar -role node -hub #{node.server.ip} -nodeConfig #{node.settings.config}"
+EOH
+end
+
+# windows_batch "ffox" do
+  # code <<-EOH
+  # cinst Firefox
+  # EOH
+# end
